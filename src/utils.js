da61405fc66439869517eed6b884595a663b9768
Anastasia R <anawachovski@mail.ru>
2023-05-24 23:38:19 +0300

    update utils

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,4 @@
 // Функция генерирующая случайное целое число
 export default function getRandomNumber(min, max) {
-  const Min = Math.ceil(min);
-  const Max = Math.floor(max);
-  return Math.floor(Math.random() * (Max - Min)) + Min; // Максимум не вкл, минимум вкл
+  return Math.floor(Math.random() * (max - min)) + min; // Максимум не вкл, минимум вкл
 }
